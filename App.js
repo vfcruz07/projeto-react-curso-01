07b3eb0efd5475ef0ec60755c68da9e7509da95a
Victor Cruz <vfcruz07@gmail.com>
2019-07-08 10:18:10 -0300

    Video #48 - Simple List Render

--- App.js
+++ App.js
@@ -3,16 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header';
 
-export default function App() {
-  return (
-    <View>
-      <Header title="Pessoas!" />      
-    </View>
-  );
+export default class App extends React.Component {
+  
+  renderList(){
 
-const styles = StyleSheet.create({
+    const names=[
+      'Eddie Van Halen',
+      'Jimi Hendrix',
+      'Chimbinha',
+      'Steve Vai',
+    ];
 
-});
+    const textElements = names.map(name =>{
+        return(<Text>{name}</Text>);
+    });
 
+    return(textElements);
+  
+  }
+  
+  render(){
+    return (
+      <View>
+        <Header title="Pessoas!" />
+        {this.renderList()}      
+      </View>
+    );
+  }
 }
 
